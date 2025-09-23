c06fe9fc1821f0551c5b1d17f9c642d87f4542d1
Veljko Bodiroga <veljko.bodiroga@gmail.com>
2025-09-24 00:02:20 +0200

    css za Pravila igre

--- src/Pravila_igre.tsx
+++ src/Pravila_igre.tsx
@@ -1,26 +1,19 @@
 import React from "react";
+import "./Pravila_igre.css";
 
 export default function PravilaIgre() {
   return (
-   <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "1fr 1fr 1fr",
-        gridTemplateRows: "auto auto",
-        gap: "20px",
-        textAlign: "center",
-      }}
-    >
+    <div className="pravila-container">
       {/* Gornja dva diva */}
-      <div style={{ gridColumn: "1 / 4", display: "flex" }}>
-        <div style={{ flex: 1 }}>
+      <div className="pravila-top">
+        <div className="pravila-box">
           <h3>Opis</h3>
           <p>
             Catan je strateška igra trgovine i izgradnje. Cilj je osvojiti 10 poena izgradnjom naselja,
             gradova i puteva te korišćenjem posebnih bonusa.
           </p>
         </div>
-        <div style={{ flex: 1 }}>
+        <div className="pravila-box">
           <h3>Cilj igre</h3>
           <p>
             Budi prvi igrač sa 10 poena, prikupljajući resurse i pametno ih razmenjujući kako bi
@@ -29,8 +22,8 @@ export default function PravilaIgre() {
         </div>
       </div>
 
-      {/* Donji red: levi, centralni, desni */}
-      <div style={{ gridColumn: "1 / 2" }}>
+      {/* Donji red */}
+      <div className="pravila-left">
         <h3>Tok poteza</h3>
         <ol>
           <li>Bacite kockice – svi igrači dobijaju resurse sa odgovarajućih polja.</li>
@@ -39,7 +32,7 @@ export default function PravilaIgre() {
         </ol>
       </div>
 
-      <div style={{ gridColumn: "2 / 3" }}>
+      <div className="pravila-center">
         <h3>Poeni</h3>
         <p>
           Naselje: 1 poen | Grad: 2 poena | Karte pobede: 1 poen <br />
@@ -47,7 +40,7 @@ export default function PravilaIgre() {
         </p>
       </div>
 
-      <div style={{ gridColumn: "3 / 4" }}>
+      <div className="pravila-right">
         <h3>Razbojnik</h3>
         <p>
           Kad se baci 7, igrači sa više od 7 karata odbacuju polovinu. Bacajući 7 pomerate
