1d3675847167bc6966688de4582f68a4211acd1d
Veljko Bodiroga <veljko.bodiroga@gmail.com>
2025-09-24 12:24:05 +0200

    pravljenje table i dodeljivanje brojeva

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Register from "./Register";
 import Stats from "./Statistika_igraca";
 import CatanInfo from "./Pravila_igre";
+import Igraj from "./Igraj"; 
 import './App.css';
 
 function App() {
@@ -10,13 +11,14 @@ function App() {
     <Router>
       <nav>
         <Link to="/">Početna</Link> | <Link to="/register">Registracija</Link> 
-        | <Link to="/stats">Statistika</Link> | <Link to="/catan">Pravila Catan</Link>
+        | <Link to="/stats">Statistika</Link> | <Link to="/catan">Pravila Catan</Link> | <Link to="/igraj">Igraj</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/stats" element={<Stats />} />
         <Route path="/catan" element={<CatanInfo />} />
+        <Route path="/igraj" element={<Igraj />} />
       </Routes>
     </Router>
   );
